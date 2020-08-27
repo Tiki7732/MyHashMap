2aac9eec199e5368bc3d86fb7a6e55fed975b21d
tiki7732 <lsaidah@gmail.com>
2020-08-27 13:25:42 -0500

    wrote the hash method for array and string

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    hash_id = 0
+    self.each_with_index do |el, ind|
+      hash_id += ind
+      hash_id *= el 
+    end
+    hash_id
   end
 end
 
 class String
   def hash
+    hash_id = 0
+    each_char.with_index do |char, ind|
+      hash_id += (ind * char.ord)
+      hash_id += self.length
+    end
+    hash_id
   end
 end
 
@@ -19,3 +31,9 @@ class Hash
     0
   end
 end
+
+# a = ("a".."z").to_a * 3
+# b = a.shuffle
+# p a.join
+# p a.join.hash
+# p b.join.hash
