76d7daa347a20ad65e86ec48c6dab564f72a2474
tiki7732 <lsaidah@gmail.com>
2020-08-31 11:30:23 -0500

    added hash_set class

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -7,17 +7,26 @@ class HashSet
   end
 
   def insert(key)
+    return if include?(key)
+    self[key.hash].push(key)
+    @count += 1
+    resize! if @count > num_buckets
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    return if !include?(key)
+    self[key.hash].delete(key)
+    @count -= 1
   end
 
   private
 
   def [](num)
+    @store[num % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -26,5 +35,14 @@ class HashSet
   end
 
   def resize!
+    nums = []
+    @store.each do |buckets|
+      buckets.each do |number|
+        nums << number
+        self.remove(number)
+      end
+    end
+    @store.concat(Array.new(num_buckets) {Array.new})
+    nums.each { |num| self.insert(num)}
   end
 end
